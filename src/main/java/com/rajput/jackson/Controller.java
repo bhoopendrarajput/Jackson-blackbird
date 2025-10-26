a073145c4a4c5d4feef3d676e0c38aabd883fcf4
bhoopendrarajput <bhoopendra.rajput@gmail.com>
2025-10-26 14:34:57 +0530

    Added alternate option to enable BlackbirdModule.

--- src/main/java/com/rajput/jackson/Controller.java
+++ src/main/java/com/rajput/jackson/Controller.java
@@ -1,15 +1,30 @@
 package com.rajput.jackson;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/")
 public class Controller {
-  @GetMapping("my-json")
+  private final ObjectMapper mapper;
+
+  @GetMapping("json")
   public ResponseEntity<HelloWorld> getJson() {
     return ResponseEntity.ok(new HelloWorld());
+  }
+
+  @GetMapping("my-json")
+  public ResponseEntity<String> getMyJson() {
+    try {
+      return ResponseEntity.ok(mapper.writeValueAsString(new HelloWorld()));
+    } catch (Exception e) {
+      e.printStackTrace();
+      return ResponseEntity.ok(new HelloWorld().toString());
+    }
   }
 }
--- src/main/java/com/rajput/jackson/JacksonConfig.java
+++ src/main/java/com/rajput/jackson/JacksonConfig.java
@@ -1,19 +1,24 @@
 package com.rajput.jackson;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.fasterxml.jackson.module.blackbird.BlackbirdModule;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@Slf4j
 public class JacksonConfig {
-
-    @Bean
-    public ObjectMapper objectMapper() {
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.registerModule(new BlackbirdModule());
-        mapper.registerModule(new JavaTimeModule());
-        return mapper;
-    }
+  //3.	Alternatively, if you want to enable it via classpath without code, create a file named:
+  @Bean
+  public ObjectMapper objectMapper() {
+    ObjectMapper mapper = new ObjectMapper();
+    mapper.registerModule(new Jdk8Module());//Optional, OptionalInt
+    mapper.registerModule(new JavaTimeModule());
+    mapper.registerModule(new BlackbirdModule());
+    log.info("Created ObjectMapper with Jdk8Module, JavaTimeModule & BlackbirdModule");
+    return mapper;
+  }
 }
